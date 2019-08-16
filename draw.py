1e9e3e7ce6d3a2295b51c7abf6e7be156d4c9c71
Erik Bernhardsson <erikbern@better.com>
2019-08-16 10:26:36 -0400

    separate cycler object

--- draw.py
+++ draw.py
@@ -50,60 +50,87 @@ class NN:
         return numpy.sum((p - q)**2, axis=1)
 
 
+class Cycler:
+    def __init__(self, method=NN, remove_bw=True, seed=None):
+        # Build matrix of 16x16x16 colors
+        vs = list(range(0, 256, 17))
+        all_rgb = []
+        for r in vs:
+            for g in vs:
+                for b in vs:
+                    all_rgb.append((r/255, g/255, b/255))
+        self.all_rgb = numpy.array(all_rgb)
+        self.m = method()
+        self.all_rgb_embedded = self.m.embed(self.all_rgb)
+        if seed is None:
+            seed = []
+        if remove_bw:
+            seed = [(0, 0, 0), (1, 1, 1)] + seed
+        self.seed = seed
+        self.seed_embedded = self.m.embed(numpy.array(seed))
+        self.remove_bw = remove_bw
+        self.colors = []
+
+    def __iter__(self):
+        self.min_dist = numpy.array([float('inf')]*len(self.all_rgb))
+        self.i = 0
+        if self.remove_bw:
+            next(self)
+            next(self)
+        return self
+
+    def __next__(self):
+        if len(self.colors) < len(self.seed_embedded):
+            # Just return the seed
+            color = self.seed[self.i]
+            last_vector_embedded = self.seed_embedded[self.i]
+        else:
+            # Find the next color
+            j = numpy.argmax(self.min_dist)
+            color = self.all_rgb[j]
+            last_vector_embedded = self.all_rgb_embedded[j]
+
+        # Update min dist for this color
+        last_vector_embedded = last_vector_embedded[numpy.newaxis,:]
+        self.min_dist = numpy.minimum(self.min_dist, self.m.dist(last_vector_embedded, self.all_rgb_embedded))
+        self.colors.append(color)
+        return color
+
+
 if __name__ == '__main__':
-    # Build matrix of 16x16x16 colors
-    vs = list(range(0, 256, 17))
-    all_rgb = []
-    for r in vs:
-        for g in vs:
-            for b in vs:
-                all_rgb.append((r/255, g/255, b/255))
-    all_rgb = numpy.array(all_rgb)
-
     for fn, obj in [
-            ('0_rgb.png', RGB()),
-            ('1_lab.png', Lab()),
-            ('2_cie94.png', CIE94()),
-            ('3_ciede2000.png', CIEDE2000()),
-            ('4_nn.png', NN())
+            ('0_rgb.png', RGB),
+            ('1_lab.png', Lab),
+            ('2_cie94.png', CIE94),
+            ('3_ciede2000.png', CIEDE2000),
+            ('4_nn.png', NN)
             ]:
-        all_rgb_embedded = obj.embed(all_rgb)
-        colors = [numpy.array(z)/0xff for z in [[0x29, 0x18, 0x42], [0x37, 0xeb, 0xc2], [0x00, 0xc3, 0x97], [0xdf, 0x16, 0x83], [0x42, 0x12, 0x87]]]
-        colors_embedded = obj.embed(numpy.array(colors))
+        cycler = Cycler(method=obj)
+        colors = []
+        for i, color in zip(range(8**2), cycler):
+            colors.append(color)
 
-        min_dist = numpy.array([float('inf')]*len(all_rgb))
-
-        for i in range(16**2):
-            if len(colors) <= i:
-                # Find the next color
-                j = numpy.argmax(min_dist)
-                print(j, numpy.max(min_dist))
-                colors.append(all_rgb[j])
-                last_vector_embedded = all_rgb_embedded[j]
-            else:
-                last_vector_embedded = colors_embedded[i]
-
-            # Update min dist for this color
-            last_vector_embedded = last_vector_embedded[numpy.newaxis,:]
-            min_dist = numpy.minimum(min_dist, obj.dist(last_vector_embedded, all_rgb_embedded))
+        # Generate Matplotlib colormap format
+        print([list(c) + [1] for c in colors])
 
         d, e = 40, 10
         k = int(numpy.ceil(len(colors)**0.5))
+        colors = numpy.reshape(colors, (k, k, 3))
         im = PIL.Image.new('RGB', (2*k*d+3*e, k*d+2*e))
         draw = PIL.ImageDraw.Draw(im)
 
         def draw_patches(colors, x_offset, y_offset):
-            for i, color in enumerate(colors):
-                x, y = i%k, i//k
-                draw.rectangle((x*d+x_offset, y*d+y_offset, (x+1)*d+x_offset, (y+1)*d+y_offset), fill=tuple(int(255*z) for z in color))
+            for row in range(colors.shape[0]):
+                for col in range(colors.shape[1]):
+                    draw.rectangle((col*d+x_offset, row*d+y_offset, (col+1)*d+x_offset, (row+1)*d+y_offset), fill=tuple(int(255*z) for z in colors[row][col]))
 
         draw_patches(colors, e, e)
-        colors = sorted(colors, key=lambda c: c[1] - c[0])
-        for o1 in range(0, len(colors), k):
-            o2 = min(o1+k, len(colors))
-            colors[o1:o2] = sorted(colors[o1:o2], key=lambda c: c[2] - c[1])
+        for it in range(110):
+            for row in range(colors.shape[0]):
+                colors[row,:] = numpy.array(sorted(colors[row,:], key=lambda c: c[1] - c[0]))
+            for col in range(colors.shape[1]):
+                colors[:,col] = numpy.array(sorted(colors[:,col], key=lambda c: c[2] - c[1]))
 
         draw_patches(colors, k*d+2*e, e)
         im.save(fn)
 
-    
