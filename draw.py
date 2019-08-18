b0abcaed429907f5a21f7863abb89b0c1a27c72f
Erik Bernhardsson <mail@erikbern.com>
2019-08-17 22:07:27 -0400

    Minor fixes

--- draw.py
+++ draw.py
@@ -45,7 +45,7 @@ class NN:
 
     def embed(self, colors):
         embedded = predict(self.params, colors, dropout=False)
-        return embedded / numpy.sum(embedded**2, axis=1)[:,numpy.newaxis]
+        return embedded / numpy.sum(embedded**2, axis=1)[:,numpy.newaxis]**0.5
 
     def dist(self, p, q):
         return numpy.sum((p - q)**2, axis=1)
@@ -59,20 +59,22 @@ class Cycler:
         for r in vs:
             for g in vs:
                 for b in vs:
-                    if not remove_bw or 17 < r+g+b < 238:
-                        all_rgb.append((r/255, g/255, b/255))
+                    all_rgb.append((r/255, g/255, b/255))
         self.all_rgb = numpy.array(all_rgb)
         self.m = method()
         self.all_rgb_embedded = self.m.embed(self.all_rgb)
         if seed is None:
-            seed = numpy.zeros((0, 3))
+            seed = numpy.eye(3)
+        else:
+            seed = numpy.array(seed)
         if remove_bw:
-            seed = numpy.array([(0, 0, 0), (1, 1, 1)] + seed)
+            seed = numpy.vstack([numpy.array([(0, 0, 0), (1, 1, 1)]), seed])
         self.seed = seed
-        self.seed_embedded = self.m.embed(numpy.array(seed))
+        self.seed_embedded = self.m.embed(seed)
         self.colors = []
         self.min_dist = numpy.array([float('inf')]*len(self.all_rgb))
         self.eps = eps
+        self.remove_bw = remove_bw
 
     def __iter__(self):
         def gen():
@@ -86,11 +88,14 @@ class Cycler:
         if isinstance(key, slice):
             return [self[i] for i in range(*key.indices(2**99))]
         assert isinstance(key, int)
+        if self.remove_bw:
+            key += 2
         while len(self.colors) <= key:
             if key < len(self.seed_embedded):
-                # Just return the seed
-                color = self.seed[key]
-                last_vector_embedded = self.seed_embedded[i]
+                # Just use the seed
+                j = len(self.colors)
+                color = self.seed[j]
+                last_vector_embedded = self.seed_embedded[j]
             else:
                 # Find the next color
                 j = numpy.argmax(self.min_dist)
@@ -102,22 +107,23 @@ class Cycler:
             self.min_dist = numpy.minimum(self.min_dist, self.m.dist(last_vector_embedded, self.all_rgb_embedded))
             self.min_dist += self.eps
             self.colors.append(color)
+
         return self.colors[key]
 
 
 if __name__ == '__main__':
     for fn, obj in [
-            ('0_rgb.png', RGB),
-            ('1_lab.png', Lab),
-            ('2_cie94.png', CIE94),
-            ('3_ciede2000.png', CIEDE2000),
-            ('4_nn.png', NN)
+            ('rgb.png', RGB),
+            ('lab.png', Lab),
+            ('cie94.png', CIE94),
+            ('ciede2000.png', CIEDE2000),
+            ('nn.png', NN)
             ]:
-        cycler = Cycler(method=obj, remove_bw=False)
+        cycler = Cycler(method=obj)
         colors = cycler[:2**8]
 
         # Generate Matplotlib colormap format
-        print([list(c) + [1] for c in colors])
+        # print([list(c) + [1] for c in colors])
 
         d, e = 40, 10
         k = int(numpy.ceil(len(colors)**0.5/2))*2
